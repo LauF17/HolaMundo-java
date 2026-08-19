71e1d443044087d8c5a38ca433e00c7aca017508
LauF17 <francodlaura@gmail.com>
2026-08-18 19:38:59 -0500

    actividad clase

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a = 10, b=3;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+    }
+}
---
+++ src/MiClase.java
@@ -0,0 +1,20 @@
+public class MiClase { 
+    private int edad = 17;
+    private double altura= 1.70;
+    private char sexo = 'f';
+    private boolean aprobado = false;
+    private String nombre = "Laura Franco";
+    private String saludo ="Hola," + nombre;
+    private double nota;
+    nota = 4.5;
+
+    private var ciudad = "Bogota";
+
+    private double precio = 9.99;
+    private int precioCortado = (int) precio;
+
+    private int entero=5;
+    private double decimal =entero;
+    
+    
+}
